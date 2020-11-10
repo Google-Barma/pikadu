5e5330a1758983f9f7d7fa5258323a9d24d6af68
Vadym <vad.evlanov@gmail.com>
2020-11-10 21:13:14 +0200

    add signin, signUp

--- js/main.js
+++ js/main.js
@@ -9,3 +9,97 @@ menuToggle.addEventListener('click', function (event) {
   // вешаем класс на меню, когда кликнули по кнопке меню
   menu.classList.toggle('visible');
 });
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignup = document.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+const listUsers = [
+  {
+    id: '01',
+    email: 'vad.evlanov@gmail.com',
+    password: 12345,
+    displayName: 'Barma',
+  },
+  {
+    id: '02',
+    email: 'testadrom@gmail.com',
+    password: 123456,
+    displayName: 'Test',
+  },
+];
+
+const setUsers = {
+  user: null,
+
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с такими данными не найден');
+    }
+  },
+
+  logOut() {
+    console.log('logOut');
+  },
+
+  signUp(email, password, handler) {
+    if (!this.getUser(email)) {
+      const user = { email, password, displayName: email };
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с таким email уже зарегистрирован');
+    }
+  },
+
+  getUser(email) {
+    return listUsers.find(item => item.email === email);
+  },
+
+  authorizedUser(user) {
+    this.user = user;
+  },
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+  console.log('user:', user);
+
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+};
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+});
+
+loginSignup.addEventListener('click', event => {
+  event.preventDefault();
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+});
+
+toggleAuthDom();
